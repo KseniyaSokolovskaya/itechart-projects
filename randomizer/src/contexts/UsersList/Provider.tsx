f2f62f54a58753538151c8cdc7d1d1414435d3fb
KseniyaSokolovskaya <deff.1990@gmail.com>
2022-02-17 12:33:38 +0300

    feat: implement check duplicate

--- randomizer/src/contexts/UsersList/Provider.tsx
+++ randomizer/src/contexts/UsersList/Provider.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import Context from './Context';
 import type { IUser, IUserListContext } from './Context';
 
@@ -7,11 +7,20 @@ const Provider: React.FC = ({ children }) => {
   const [lastStr, setLastStr] = useState('');
   const [listStr, setLisStr] = useState<string[]>([]);
 
+
+  function isDuplicateName(user: IUser) {
+    return !!userList.find((item: IUser) => item.lastName === user.lastName && item.firstName === user.firstName);
+  }
+
   function changeUserList(user: IUser) {
-    setUserList((prev) => [...prev, user]);
-    const str = `${user.firstName} ${user.lastName}, ${user.occupation} from ${user.country}`;
-    setLastStr(str);
-    setLisStr((prev) => [...prev, str]);
+    if (!isDuplicateName(user)) {
+      setUserList((prev) => [...prev, user]);
+
+      const str = `${user.firstName} ${user.lastName}, ${user.occupation} from ${user.country}`;
+
+      setLastStr(str);
+      setLisStr((prev) => [...prev, str]);
+    }
   }
 
   const contextValue: IUserListContext = { userList, lastStr, listStr, changeUserList };
