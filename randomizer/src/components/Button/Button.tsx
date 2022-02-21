e11ff778c9efd18704a8a177b2f65144ab7a6f1c
kseniyasokolovskaya <deff.1990@gmail.com>
2022-02-21 11:23:45 +0300

    fix: fix bug related with limit request

--- randomizer/src/components/Button/Button.tsx
+++ randomizer/src/components/Button/Button.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useUserListContext from '../../contexts/UsersList';
 import { IUserProps } from '../../interfaces';
 import './index.scss';
 
 const Button: React.FC = () => {
   const userList = useUserListContext();
+
+  const [isDisabledBtn, setDisabledBtn] = useState(false);
 
   function getRandomCountry(arr: string[]) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -15,21 +17,28 @@ const Button: React.FC = () => {
   }
 
   async function handleClick() {
-    const requestUsers = fetch('https://rnd-test-api.herokuapp.com/api/v1/persons?count=5');
-    const requestCountries = fetch('https://rnd-test-api.herokuapp.com/api/v1/countries?count=5');
+    setDisabledBtn(true);
 
-    const [usersResult, countriesResult] = await Promise.all([requestUsers, requestCountries]);
+    if (userList.counter < 10) {
+      const requestUsers = fetch('https://rnd-test-api.herokuapp.com/api/v1/persons?count=5');
+      const requestCountries = fetch('https://rnd-test-api.herokuapp.com/api/v1/countries?count=5');
 
-    const countries = await countriesResult.json();
-    const users = await usersResult.json();
+      const [usersResult, countriesResult] = await Promise.all([requestUsers, requestCountries]);
 
-    const randomUser = getRandomUser(users);
-    const randomCountry = getRandomCountry(countries);
-    userList.changeUserList({ ...randomUser, country: randomCountry });
+      const countries = await countriesResult.json();
+      const users = await usersResult.json();
+
+      const randomUser = getRandomUser(users);
+      const randomCountry = getRandomCountry(countries);
+
+      userList.changeUserList({ ...randomUser, country: randomCountry });
+
+      setDisabledBtn(false);
+    }
   }
 
   return (
-    <button className="btn" onClick={handleClick} disabled={userList.counter === 10}>
+    <button className="btn" onClick={handleClick} disabled={isDisabledBtn}>
       Generate
     </button>
   );
